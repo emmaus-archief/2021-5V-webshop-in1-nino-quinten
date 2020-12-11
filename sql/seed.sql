a7357363e2a43fa720c0cc8417036cc81196b8b2
Qwinsnator <quintenman4@gmail.com>
2020-12-11 10:04:40 +0000

    s

--- sql/seed.sql
+++ sql/seed.sql
@@ -3,15 +3,15 @@
 --
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
-insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Jordan 4 Retro Bred', 'goeie schoen, past mooi, niet te duur, alles alles', 350, 'rood-zwart', 4-5-19, 308497060, 'Jordan 4','Jordan');
-/*insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Jordan 1 Retro High Bred Toe', 'goeie schoen, past mooi, niet te duur, alles alles', 600, 'rood-zwart', 24-2-18, 555088610, 'Jordan 1','Jordan');
-insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Jordan 11 Retro Playoffs Bred', 'goeie schoen, past mooi, niet te duur, alles alles', 300, 'rood-zwart', 14-12-19, 378037061, 'Jordan 11','Jordan');
-insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Jordan 1 Retro High Royal Toe', 'goeie schoen, past mooi, niet te duur, alles alles', 330, 'blauw-zwart', 9-5-20, 555088041, 'Jordan 1','Jordan');
+insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Jordan 4 Retro Bred', 'Classic schoenen', 350, 'rood-zwart', 4-5-19, 308497060, 'Jordan 4','Jordan');
+/*insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Jordan 1 Retro High Bred Toe', 'Mooie schoenen met veel hype', 600, 'rood-zwart', 24-2-18, 555088610, 'Jordan 1','Jordan');
+insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Jordan 11 Retro Playoffs Bred', 'Nice pattas', 300, 'rood-zwart', 14-12-19, 378037061, 'Jordan 11','Jordan');
+insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Jordan 1 Retro High Royal Toe', 'Clean schoenen', 330, 'blauw-zwart', 9-5-20, 555088041, 'Jordan 1','Jordan');
 insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Jordan 1 Retro High Off White Blue', All time grail', 2000, 'blauw-wit', 23-6-18, 878236488, 'Jordan 1','Jordan');
 
 insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Yeezy 350 V2 Black Red', 'clown pattas', 280, 'rood-zwart', 5-12-20, 193758463, 'Yeezy 350','Yeezy');
 insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Yeezy 350 V2 Zebra', 'Mooie schoenen', 300, 'wit-zwart', 25-2-17, 837201847, 'Yeezy 350','Yeezy');
-insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Yeezy 350 V2 Cloud White', 'goeie schoen, past mooi, niet te duur, alles alles', 400, 'wit', 21-9-19, 264813640, 'Yeezy 350','Yeezy');
+insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Yeezy 350 V2 Cloud White', 'Past bij elk outje', 400, 'wit', 21-9-19, 264813640, 'Yeezy 350','Yeezy');
 insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Yeezy 500 Stone', 'Confortabele schoenen', 350, 'stone', 23-11-19, 468274828, 'Yeezy 500','Yeezy');
 insert into products (name, description, price, colorway, release_date, code, silhouette, brand) values ('Yeezy 700 Wave Runner', 'Chuncky schoenen die erg lekker lopen', 500, 'grijs-wit', 1-11-17, 264910364, 'Yeezy 700','Yeezy');
 
